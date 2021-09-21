7cd1785b8447d8498c87da4b55dfa632608d69e7
Avinash S <avinashthebigboss@gmail.com>
2021-09-21 16:23:49 +0530

    EDITED index.html style.css script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 "use strict";
+// SLIDER
+
+const slider = document.getElementById('grid-range');
+const output = document.getElementById('grid-size');
+const gridBtn = document.getElementById('grid-btn');
+const gridContainer = document.getElementById('grid-container');
+const gridItem = document.createElement('div');
+
+
+
+
+output.textContent = `GRID SIZE : ${slider.value} x ${slider.value}`;
+
+slider.oninput = function () {
+    output.textContent = `GRID SIZE : ${this.value} x ${this.value}`;
+
+    for (let i = 0; i < this.value ** 2; i++) {
+        const gridItem = document.createElement('div');
+        gridContainer.style.gridTemplate = `repeat(${this.value},1fr)/repeat(${this.value},1fr)`;
+
+        gridBtn.addEventListener('click', function style() {
+            gridItem.classList.remove('grid');
+            gridItem.classList.add('grid');
+            gridContainer.appendChild(gridItem);
+        })
+
+    }
+
+
+}
+
+//
+
+
