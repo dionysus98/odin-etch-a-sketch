ebd7e213cf4d8fc871125b43cee7b3be34e7b00f
Avinash S <avinashthebigboss@gmail.com>
2021-09-22 15:20:33 +0530

    EDITED index.html style.css script.cs (inWindows)

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 "use strict";
-// SLIDER
 
+// SLIDER
 const slider = document.getElementById('grid-range');
 const output = document.getElementById('grid-size');
+
+//GRID
 const gridBtn = document.getElementById('grid-btn');
 const gridContainer = document.getElementById('grid-container');
-const gridItem = document.createElement('div');
 
+//COLOR
+const color = document.getElementById('color');
+const colorValue = document.getElementById('color-value');
+console.log(color.value);
+//RESET
+const reset = document.getElementById('reset');
+const eraser = document.getElementById('eraser');
+
+//CAT
+const catImg = document.getElementById('img');
+const catText = document.getElementById('cat');
 
 
 
@@ -16,16 +28,45 @@ gridBtn.addEventListener('click', function () {
 
     slider.oninput = function () {
         output.textContent = `GRID SIZE : ${this.value} x ${this.value}`;
-
     }
+
     for (let i = 0; i < slider.value ** 2; i++) {
         const gridItem = document.createElement('div');
         gridContainer.style.gridTemplate = `repeat(${slider.value},1fr)/repeat(${slider.value},1fr)`;
+        gridItem.setAttribute('id', 'grid-item');
         gridItem.classList.add('grid');
         gridContainer.appendChild(gridItem);
+
+        reset.addEventListener('click', function () {
+            gridContainer.removeChild(gridItem);
+        })
+
+        gridItem.addEventListener('mouseover', function () {
+            gridItem.style.backgroundColor = 'black';
+        })
+
+        eraser.addEventListener('click', function () {
+            gridItem.addEventListener('mouseover', function () {
+                gridItem.style.backgroundColor = 'white';
+            })
+        })
     }
 });
 
-//
+
+
+
+
+
+
+catImg.addEventListener("click", function () {
+    catText.textContent = "DON'T WAKE HER UP!";
+
+})
+
+
+
+
+
 
 
